da5116580367d83817b48d2b84d4cb3721055882
Jinhu Wang <52737000+Jinhu-Wang@users.noreply.github.com>
2020-03-12 16:01:15 +0800

    Update FoxTree.h

--- TreeSeparation/TreeSeparation/FoxTree.h
+++ TreeSeparation/TreeSeparation/FoxTree.h
@@ -1,3 +1,26 @@
+/*
+*	Copyright (C) 2016 by
+*       Jinhu Wang (jinhu.wang@tudelft.nl)
+*       Roderik Lindenbergh (r.c.lindenbergh@tudelft.nl) [http://doris.tudelft.nl/~rlindenbergh/]
+*       Laser and Optical Remote Sensing
+*	Dept. of Geoscience and Remote Sensing
+*	TU Delft, https://3d.bk.tudelft.nl
+*
+*
+*	This is free software; you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License Version 3
+*	as published by the Free Software Foundation.
+*
+*	TreeSeparation is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*	GNU General Public License for more details.
+*
+*	You should have received a copy of the GNU General Public License
+*	along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #pragma once
 
 
